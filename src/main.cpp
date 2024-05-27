f23d8cfb1790e1c7763f2dad3dc62c07a42abecd
lanshengzhi <lanshengzhi@gmail.com>
2024-05-27 16:55:17 +0900

    MQTT topic publication and subscription test passed

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,37 @@
 #include "PushButton.h"
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <DHTesp.h>
+#include <SimpleTimer.h>
+
+// WiFI
+static const char wifi_ssid[]       = "YOU WIFI SSID";
+static const char wifi_password[]   = "YOU WIFI PASSWORD";
+
+// MQTT broker
+static const char mqtt_broker_ip[]  = "YOU MQTT Broker's IP address";
+const int  mqtt_broker_port         = 1883;
+static const char mqtt_broker_username[] = "mqtt_broker_username";
+static const char mqtt_broker_password[] = "mqtt_broker_password";
+
+// MQTT topic
+static const char topic_publish[]   = "ESP32/data";
+static const char topic_subscribe[] = "ESP32/command";
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+DHTesp dht;
+SimpleTimer timer;
 
 PushButton prevButton(32);
 PushButton upButton(33);
 PushButton downButton(25);
 PushButton nextButton(26);
-
-void OnPrevButtonPressed() {
-    Serial.println("OnPrevButtonPressed");
-}
-
-void OnPrevButtonReleased() {
-    Serial.println("OnPrevButtonReleased");
-}
 
 void OnUpButtonPressed() {
     Serial.println("OnUpButtonPressed");
@@ -33,8 +49,59 @@ void OnNextButtonReleased() {
     Serial.println("OnNextButtonReleased");
 }
 
+#define MSG_BUFFER_SIZE	(50)
+static char publish_msg[MSG_BUFFER_SIZE];
+
+void TimerCallback() {
+    TempAndHumidity tempAndHum = dht.getTempAndHumidity();
+    Serial.printf("Temperature: %f\n", tempAndHum.temperature);
+    Serial.printf("Humidity: %f\n",  tempAndHum.humidity);
+    snprintf(publish_msg, MSG_BUFFER_SIZE, "temp: %f, Humidiy: %f", tempAndHum.temperature, tempAndHum.humidity);
+    mqttClient.publish(topic_publish, publish_msg);
+}
+
+// 
+void MQTTCallback(const char* topic, uint8_t* payload, unsigned int length) {
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+
+    for (int i = 0; i < length; i++) {
+        Serial.print((char)payload[i]);
+    }
+
+    Serial.println();
+}
+
 void setup() {
     Serial.begin(921600); 
+
+    dht.setup(27, DHTesp::DHT11);
+
+    WiFi.begin(wifi_ssid, wifi_password);
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(2000);
+        Serial.println("Connecting to the wifi......");
+    }
+
+    Serial.println("WiFI connected");
+
+    mqttClient.setServer(mqtt_broker_ip, mqtt_broker_port);
+
+    while (!mqttClient.connected()) {
+        String client_id = "ESP32-TEST-1";
+        if (mqttClient.connect(client_id.c_str(), mqtt_broker_username, mqtt_broker_password)) {
+            Serial.println("MQTT server connected");
+        } else {
+            Serial.println("MQTT server connect fail: " + String(mqttClient.state()));
+        }
+        delay(2000);
+    }
+
+    // Subscribe the topic
+    mqttClient.subscribe(topic_subscribe);
+    mqttClient.setCallback(MQTTCallback);
 
     lcd.init();
     lcd.backlight();
@@ -44,15 +111,14 @@ void setup() {
     downButton.Begin();
     nextButton.Begin();
 
-    //prevButton.SetPressedCallback(OnPrevButtonPressed);
-    //prevButton.SetReleasedCallback(OnPrevButtonReleased);
-
     prevButton.SetPressedCallback([]() { lcd.clear(); lcd.print("Prev BTN D"); });
     prevButton.SetReleasedCallback([]() { lcd.clear(); lcd.print("Prev BTN U"); });
 
     upButton.SetPressedCallback(OnUpButtonPressed);
     downButton.SetPressedCallback(OnDownButtonPressed);
     nextButton.SetPressedCallback(OnNextButtonReleased);
+
+    timer.setInterval(2000, TimerCallback);
 }
 
 void loop() {
@@ -60,4 +126,6 @@ void loop() {
     upButton.Run();
     downButton.Run();
     nextButton.Run();
+    mqttClient.loop();
+    timer.run();
 }
